449c159d10dd4a6337fcc8837d9fea507aff242d
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-06 19:03:57 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,7 +9,7 @@ class LandmarksController < ApplicationController
     redirect("/landmarks/#{@landmark.id}")
   end
 
-  get :'/landmarks/:id' do
+  get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     erb :'landmarks/show'
   end
