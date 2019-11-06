42a837e438296dce93b43b8c2f16d4aa3bc7fc30
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-06 18:55:05 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,11 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/new'
   end
 
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+
+    redirect("/landmarks/#{@landmark.id}")
+
+  end
+
 end
